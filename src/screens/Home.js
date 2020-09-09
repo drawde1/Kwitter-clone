66d84a163fe91e8d73ad55b023958e77334df8fa
Charles Campbell <wbsbpd88@gmail.com>
2020-09-09 09:38:49 -0400

    Added Code From Master

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,9 +4,7 @@ import { LoginFormContainer, MenuContainer } from "../components";
 export const HomeScreen = () => (
   <>
     <MenuContainer />
-    <center>
-      <h2>Your favorite microblogging platform</h2>
-    </center>
+    <h2>Your favorite microblogging platform</h2>
     <LoginFormContainer />
   </>
 );
