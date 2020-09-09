14c38ac15421a616ce26b425f9a138ce95c766c6
Shanquetta Pelzer <shanquetta.pelzer@outlook.com>
2020-09-09 14:39:19 -0500

    Working master now

--- src/components/feed/Message.js
+++ src/components/feed/Message.js
@@ -1,16 +1,17 @@
 import React, { useState, useRef }  from 'react'
-import { createTimestamp } from '../../functions/createTimestamp'
+import { createTimestamp } from '../../components/functions/createTimestamp'
 import { useEffect,} from 'react'
 import {  useDispatch,useSelector} from "react-redux";
-import {addLike, deleteLike, deleteMessage, getMessageList} from '../../redux/actions/messages'
+import {addLike, deleteLike, deleteMessage} from '../../redux/actions/messages'
 import actions from 'redux-form/lib/actions';
 import {likes} from '../../redux/actions/messages'
+
 
 export const Message = (props) =>
 {
 //TODO: handle delete message
 //TODO: handle likes add & delete
-
+const dispatch =useDispatch()
 
     // id: 0,
     // text:'',
@@ -19,24 +20,41 @@ export const Message = (props) =>
     // likes:[],
     
     // 2020-09-03T14:27:16.454Z
+
     const msgListParams =
-  {
-    limit: 10,
-    offset: 0
-  }
-  const dispatch = useDispatch()
-const handleDelete = () =>
-{
-    console.log(props.msgId)
-    dispatch(deleteMessage(props.msgId))
-    dispatch(getMessageList(msgListParams))
-}
+    {
+        limit: 10,
+        offset: 0
+    }
+
+    
+    const handleDelete = () =>
+    {
+        console.log(props.messageId)
+        dispatch(deleteMessage(props.msgId))
+    }
+
  let timestamp = createTimestamp(props.createdAt)
 // // useEffect(()=>{})
 // componentDidMount(()=>{timestamp = })
 
- return (  
-    <>
+ 
+  const [disClick, setDisClick] = useState(0)
+  const [deselect, setDeselect] = useState(false)
+
+  const handleLike = (messageId) => {
+      dispatch(likes(messageId))
+
+      //dispatch(getMessageList(msgListParams));
+  }
+
+//   const handleUnlike = messageId => () => {
+//       console.log(messageId);
+//       this.props.dispatch(deleteLike(messageId))
+//   }
+  //const [likeMessage] = likes(this.props.messageId)
+
+ return (  <>
         <p>user:{props.username}</p>
         <p>text:{props.text}</p>
         <p>time: {timestamp.time}<span> date:{timestamp.date}</span></p>
@@ -47,15 +65,16 @@ const handleDelete = () =>
             <a class="ui basic blue left pointing label">
                {likes.length} 
             </a>
-            </div>
-            <div class="ui labeled button" tabIndex="0">
-                <button class="ui red button" onClick={() => setDisClick(disClick + 1 )}>
-                    <i class="frown icon"></i> Dislikes
-                </button>
-                <a class="ui basic left pointing red label">
-                    {disClick}
-                </a>
-            </div>
-        <button onClick ={()=>handleDelete()}>x</button>
+        </div>
+        <div class="ui labeled button" tabIndex="0">
+            <button class="ui red button" onClick={() => setDisClick(disClick + 1 )}>
+                <i class="frown icon"></i> Dislikes
+            </button>
+            <a class="ui basic left pointing red label">
+                {disClick}
+            </a>
+        </div>
+        
+
     </>)
 }
--- src/redux/actions/messages.js
+++ src/redux/actions/messages.js
@@ -1,47 +1,38 @@
 import api from "../../utils/api";
 import { FAILURE } from "./defaultuser";
-export const DELETE_MESSAGE = "DELETE_MESSAGE";
-export const ADD_MESSAGE = "ADD_MESSAGE";
-export const GET_MESSAGE_LIST = "GET_MESSAGE_LIST";
+
+export const DELETE_MESSAGE = 'DELETE_MESSAGE';
+export const ADD_MESSAGE= 'ADD_MESSAGE'
 export const ADD_LIKE = 'ADD_LIKE'
+export const DELETE_LIKE = 'DELETE_LIKE'
+export const GET_MESSAGE_LIST = 'GET_MESSAGE_LIST'
+export const LIKE_FAILURE = 'LIKE_FAILURE'
 
-export const addMessage = text => async (dispatch, getState) => {
-  try {
-    const payload = await api.addMessage(text);
-    dispatch({ type: ADD_MESSAGE, payload });
-  } catch (err) {
-    dispatch({
-      type: FAILURE,
-      payload: err.message,
-    });
-  }
-};
+export const addMessage= (text) => async (dispatch, getState) => {
+    try {
+      
+      const payload = await api.addMessage(text);
+        
+      dispatch({ type: ADD_MESSAGE, payload });
+    } catch (err) {
+      dispatch({
+        type: FAILURE,
+        payload: err.message,
+      });
+    }
+  };
 
-export const getMessageList = msgParams => async (dispatch, getState) => {
-  try {
-    const payload = await api.getMessageList(msgParams);
-    dispatch({ type: GET_MESSAGE_LIST, payload });
-  } catch (err) {
-    dispatch({
-      type: FAILURE,
-      payload: err.message,
-    });
-  }
-};
-
-// export const deleteMessage = messageid => async (dispatch, getState) => {
-//   try {
-//     const payload = await api.deleteMessage(messageid);
-//     console.log(payload);
-//     dispatch({ type: DELETE_MESSAGE, payload });
-//   } catch (err) {
-//     dispatch({
-//       type: FAILURE,
-//       payload: err.message,
-//     });
-//   }
-// };
-
+  export const getMessageList = (msgParams) => async (dispatch, getState) => {
+    try {
+      const payload = await api.getMessageList(msgParams);
+      dispatch({ type: GET_MESSAGE_LIST, payload });
+    } catch (err) {
+      dispatch({
+        type: FAILURE,
+        payload: err.message,
+      });
+    }
+  };
 
 export const deleteMessage = messageid => async (dispatch, getState) => {
   try {
@@ -95,19 +86,3 @@ export const deleteLikes = (messageId) => async (dispatch, getState) => {
     });
   }
 };
-
-
-// export const addLike = (messageId) => async (dispatch, getState) {
-//   try {
-//   return {
-//     type: ADD_LIKE,
-//     payload: { message, user }
-//   };
-// };
-
-// export const deleteLike = message => {
-//   return {
-//     type: DELETE_LIKE,
-//     payload: message
-//   };
-// };
