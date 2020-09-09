e64f582a24bbdaaa58d3f062cb760df2d4840a20
Charles Campbell <wbsbpd88@gmail.com>
2020-09-09 09:47:02 -0400

    Update

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { LoginFormContainer, MenuContainer } from "../components";
-
 export const HomeScreen = () => (
   <>
     <MenuContainer />
     <h2>Your favorite microblogging platform</h2>
-    <LoginFormContainer />
+    {/* <LoginFormContainer /> */}
   </>
 );
