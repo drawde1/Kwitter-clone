4d705573912679b873d457f40f24dbaf31ec12ff
Tuan Wright <tawright28@gmail.com>
2020-09-11 11:11:17 -0400

    testing

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,72 +1,151 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actions } from "../../redux/actions/auth";
+import { Loader } from "../loader";
 import "./Profile.css";
-import {user} from "../../redux/actions/user";
-
+import {updateuser} from "../../redux/actions/user";
+import { getPicture } from "../../redux/actions/photos";
+import { useRef } from "react";
+import Api from "../../utils/api"
+import { getUserInfo} from '../../redux/actions'
+import {Message} from '../feed/Message'
+import {infiniteScroll} from '../../redux/actions/infiniteScroll'
+import {restInfiniteScroll} from '../../redux/actions/infiniteScroll'
+import {getMessageListByUser} from '../../redux/actions'
+import "./scrollbox.css"
+import {deleteUser} from '../../redux/actions/user'
 
 export const Profile = () => {
-    const {username, displayname} = useSelector((state) => ({
-        username: state.adduser.username,
-        name: state.adduser.displayname
-    }));
+  const INITIALSTATE = {
+    // username: "",
+    displayName: "",
+    about: "",
+    password: "",
+  };
 
-    const dispatch = useDispatch();
+  const [state, setState] = useState({ INITIALSTATE });
 
-    const INITIALSTATE = {
-        username: "",
-        displayname: "",
-        about: ""
+  const {
+    username,
+    startingUsername,
+    userPicture,
+    messageList,
+    bio,
+    name,
+    msgListParams,
+    testPicture,
+    count,
+    userInfo,
+  } = useSelector(state => ({
+    startingUsername: state.auth.username,
+    testPicture: state.pic.photo,
+    username: state.getUser.username,
+    userPicture: state.getUser.pictureLocation,
+    userInfo:  state.getUser,
+    name: state.getUser.displayName,
+    bio: state.getUser.about,
+    count: state.getMessageList.count,
+    messageList: state.getMessageListByUser.messages,
+    msgListParams: state.infiniteScroll,
+  }))
+
+  const dispatch = useDispatch();
+  const picture = useRef(null);
+
+  useEffect(()=>{dispatch(getMessageListByUser(msgListParams,username))},[])
+  useEffect(()=>{dispatch(getUserInfo(startingUsername))},[])
+  useEffect(()=>{dispatch(restInfiniteScroll(10))},[])
+
+  const addPic = async (event) => {
+    event.preventDefault();
+  
+    const picdata = new FormData (picture.current)
+    const results = await Api.addPicture( username, picdata )
+    dispatch(getUserInfo(username))
+  };
+
+  
+  const deleteTheUser = () => {
+    dispatch(deleteUser(username));
+    console.log('??')
+  };
+ 
+  
+  
+  useEffect(()=>{
+    dispatch(getMessageListByUser(msgListParams,username))
+  
+  },[])
+  
+  const setPic = async event => {
+    event.preventDefault();
+    const picdata = new FormData(picture.current);
+    const results = await Api.getPictures(username, picdata);
+  };
+  const handleScroll = event => {
+    const { scrollHeight, clientHeight, scrollTop } = event.currentTarget;
+    if (clientHeight + scrollTop >= scrollHeight) {
+      dispatch(infiniteScroll(5));
+      dispatch(getMessageListByUser(msgListParams, username));
     }
+  };
+  const test = () =>
+  {
+    dispatch(getPicture(username))
+    console.log(testPicture)
+  }
+  
+  return (
 
-    const [state, setState] = useState({INITIALSTATE});
+    <React.Fragment>
+      <h1>Profile Page</h1>
+      
+     <button onClick= {test}>test</button>
+      <form ref={picture} onSubmit={addPic}>
+        <input type='file' name='picture'></input>
+        <button type='submit'>Upload My Picture</button>
+      </form>
+ 
+        <h1>Profile Page</h1>
+        <div class="ui card">
+          <div class="image">
+            <img 
+          src = {userPicture?"https://kwitter-api.herokuapp.com" + userPicture:'/kwitter-user.png'}
+          width="200" 
+          height="200"/>
+        </div>
 
-    const handleUpdate = (event) => {
-        event.preventDefault();
-        dispatch(actions.user(state));
-    }
-
-    const handleChange = (event) => {
-        const inputName = event.target.name;
-        const inputValue = event.target.value;
-        setState((prevState) => ({ ...prevState, [inputName]: inputValue }));
-    };
-
-    return (
-        <React.Fragment>
-            <form id="updat-form" onSubmit={handleUpdate}>
-                <label htmlFor="username">Current Username: {username}</label>
-                <input
-                type="text"
-                name="username"
-                value={state.username}
-                autoFocus
-                required
-                onChange={handleChange}
-                />
-                <label htmlfor="displayname">Current Name: {this.name} </label>
-                <input
-                type="text"
-                name="displayname"
-                value={state.displayname}
-                autoFocus
-                required
-                onChange={handleChange}
-                />
-                <label htmlfor="displayname">bio</label>
-                <input
-                type="text"
-                name="about"
-                value={state.about}
-                autoFocus
-                required
-                onChange={handleChange}
-                />
-
-                <button type="submit">
-                    Update Profile
-                </button>
-            </form>
-        </React.Fragment>
-    )
-}
+      <div class="content">
+        <a class="header">{name}</a>
+        <div class="meta">
+          <span class="date">SE April 2020</span>
+        </div>
+        <div class="description">
+          {bio}
+        </div>
+      </div>
+      <div class="extra content">
+        <a>
+          <i class="envelope icon"></i>
+          {count}
+        </a> 
+            <button class="ui right floated button" onClick ={() => deleteTheUser()}>Delete Account</button>
+      </div>
+      </div>
+        
+        <h2>your messages</h2>
+        <div className= 'scrollBox' onScroll ={handleScroll}>
+          {messageList.map((message) => (
+                  <Message text={message.text} 
+                  username={message.username}
+                  msgId ={message.id}
+                  key = {message.id} 
+                  likes = {message.likes}
+                  createdAt ={message.createdAt}
+                  profile = {true}
+                  />
+                  ))}
+        </div>
+    </React.Fragment>
+  );
+  }
