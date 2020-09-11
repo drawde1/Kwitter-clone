d281f3f34b9d68c75a4844b7582a1230bb698a37
Elijah Camara <elijahjohncamara@icloud.com>
2020-09-11 09:35:33 -0400

    breaking my profile so i cant commit.

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,140 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actions } from "../../redux/actions/auth";
-import { Loader } from "../loader";
 import "./Profile.css";
-import {updateuser} from "../../redux/actions/user";
-import { getPicture } from "../../redux/actions/photos";
-import { useRef } from "react";
-import Api from "../../utils/api"
-import { getUserInfo} from '../../redux/actions'
-//import "./LoginForm.css";
-import {Message} from '../feed/Message'
-import {getMessageList} from '../../redux/actions'
-import "./scrollbox.css"
+import {user} from "../../redux/actions/user";
+
+
 export const Profile = () => {
-  const INITIALSTATE = {
-    // username: "",
-    displayName: "",
-    about: "",
-    password: ""
-  }
-  const [state, setState] = useState({INITIALSTATE});
-  const{username,userPicture,userInfo,messageList, name, bio } = useSelector((state)=>
-  ({
-    username: state.auth.username,
-    userPicture: state.getUser.pictureLocation,
-    userInfo:  state.getUser,
-    messageList: state.getMessageList.messages,
-    name: state.getUser.displayName,
-    bio: state.getUser.about
-  }))
-  console.log(state)
-  const dispatch = useDispatch();
-  const picture = useRef(null);
-  const msgListParams =
-  {
-    limit: 10,
-    offset: 0
-  }
-  useEffect(()=>{dispatch(getMessageList(msgListParams))},[])
-  useEffect(()=>{dispatch(getUserInfo(username))},[])
-  // const handleGetUser = (username) =>
-  const addPic = async (event) => {
-    event.preventDefault();
-    const picdata = new FormData (picture.current)
-    const results = await Api.addPicture( username, picdata )
-    dispatch(getUserInfo(username))
-  };
-  const yourMessages = messageList.filter((message)=>message.username === username)
-  useEffect(()=>{
-    dispatch(getMessageList(msgListParams))
-  },[])
-const handleUpdate = (event) => {
-    event.preventDefault();
-    dispatch(updateuser({...state,username}));
-}
-const handleChange = (event) => {
-    const inputName = event.target.name;
-    const inputValue = event.target.value;
-    setState((prevState) => ({ ...prevState, [inputName]: inputValue }));
-};
-   const setPic = async (event) => {
-     event.preventDefault();
-     //setState((prevState) => ({ ...prevState, formData: new FormData (picture)}));
-     //dispatch(setPicture(state))
-     const picdata = new FormData (picture.current)
-      const results = await Api.getPictures( username, picdata )
-      console.log(picdata)
-      console.log(results)
-   };
-  return (
-    <React.Fragment>
-        <h1>Profile Page</h1>
-        <img 
-        src = {"https://kwitter-api.herokuapp.com"+userPicture}
-        width="200" 
-        height="200"/>
-      <form ref={picture} onSubmit = {addPic}>
-       <input type="file" name="picture">
-        </input>
-        <button type="submit">upload picture</button> 
-      </form>
-      {/* <button  onClick={addPicChange}>Change Picture</button> */}
-        <form id="update-form" onSubmit={handleUpdate}>
-                <div>Current Name: {name}</div>
-                <label htmlFor="displayName">New Name:</label>
+    const {username, displayname} = useSelector((state) => ({
+        username: state.adduser.username,
+        name: state.adduser.displayname
+    }));
+
+    const dispatch = useDispatch();
+
+    const INITIALSTATE = {
+        username: "",
+        displayname: "",
+        about: ""
+    }
+
+    const [state, setState] = useState({INITIALSTATE});
+
+    const handleUpdate = (event) => {
+        event.preventDefault();
+        dispatch(actions.user(state));
+    }
+
+    const handleChange = (event) => {
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+        setState((prevState) => ({ ...prevState, [inputName]: inputValue }));
+    };
+
+    return (
+        <React.Fragment>
+            <form id="updat-form" onSubmit={handleUpdate}>
+                <label htmlFor="username">Current Username: {username}</label>
                 <input
-                    type="text"
-                    name="displayName"
-                    value={state.displayName}
-                    autoFocus
-                    required
-                    onChange={handleChange}
+                type="text"
+                name="username"
+                value={state.username}
+                autoFocus
+                required
+                onChange={handleChange}
                 />
-                <br/>
-                <div>Current Password: {}</div>
-                <label htmlFor="password">New Password:</label>
+                <label htmlfor="displayname">Current Name: {this.name} </label>
                 <input
-                    type="text"
-                    name="password"
-                    value={state.password}
-                    autoFocus
-                    required
-                    onChange={handleChange}
+                type="text"
+                name="displayname"
+                value={state.displayname}
+                autoFocus
+                required
+                onChange={handleChange}
                 />
-                <br/>
-                <div>Current Bio: {bio}</div>
-                <label htmlFor="about">New Bio:</label>
+                <label htmlfor="displayname">bio</label>
                 <input
-                  type="text"
-                  name="about"
-                  value={state.about}
-                  autoFocus
-                  required
-                  onChange={handleChange}
+                type="text"
+                name="about"
+                value={state.about}
+                autoFocus
+                required
+                onChange={handleChange}
                 />
-                <br/>
-                <button onClick={()=>updateuser(state.displayName, state.password, state.about, username)} type="submit">
-                Update Profile
+
+                <button type="submit">
+                    Update Profile
                 </button>
             </form>
-        <div>{state.displayName}</div>
-        <div>{state.password}</div>
-        <div>{state.bio}</div>
-        <div>{userPicture}</div>
-        <h2>your messages</h2>
-        <div className= 'scrollBox'>
-          {yourMessages.map((message) => (
-                  <Message text={message.text} 
-                  username={message.username}
-                  msgId ={message.id}
-                  key = {message.id} 
-                  likes = {message.likes}
-                  createdAt ={message.createdAt}
-                  />
-                  ))}
-        </div>
-    </React.Fragment>
-  );
-  }
+        </React.Fragment>
+    )
+}
