6f10e3d4271fefaf300ebfe629e13eadd44c414a
root <root@DESKTOP-A4888S4.localdomain>
2021-04-30 12:31:06 -0400

    fixed picture bugs and added home page and deployment scripts to packag.json

--- src/components/feed/Message.js
+++ src/components/feed/Message.js
@@ -7,113 +7,113 @@ import {
   profileLikes,
 } from "../../redux/actions/messages";
 import { likes } from "../../redux/actions/messages";
-import {profileDeleteMessage,profileDeleteLikes} from '../../redux/actions/messages'
+import { profileDeleteMessage, profileDeleteLikes } from '../../redux/actions/messages'
 export const Message = props => {
- 
+
   const dispatch = useDispatch();
 
   const messagListParams = useSelector(state => state.infiniteScroll);
 
-    const{ msgListParams,username } = useSelector((state)=>
-    ({
-      username: state.getUser.username,
-      msgListParams: state.infiniteScroll,
-    }))
+  const { msgListParams, username } = useSelector((state) =>
+  ({
+    username: state.getUser.username,
+    msgListParams: state.infiniteScroll,
+  }))
 
   const handleDelete = () => {
-    if(props.profile)
-    {
-      dispatch(profileDeleteMessage(props.msgId, messagListParams,username));
+    if (props.profile) {
+      dispatch(profileDeleteMessage(props.msgId, messagListParams, username));
     }
-    else 
-    {
+    else {
       dispatch(deleteMessage(props.msgId, messagListParams));
     }
   };
 
   let timestamp = createTimestamp(props.createdAt);
-  
+
   const [disClick, setDisClick] = useState(13);
 
   const handleLike = messageId => {
     dispatch(likes(messageId, msgListParams));
-    if (!props.profile) { dispatch(likes(messageId, msgListParams));
+    if (!props.profile) {
+      dispatch(likes(messageId, msgListParams));
     } else {
       dispatch(profileLikes(messageId, msgListParams, username));
     }
   };
-  
+
   let yourLike
   const handleUnlike = () => {
     for (let like of props.likes) {
       if (like.username === username) {
         yourLike = like.id
-      }   
-  }
-  if (!props.profile) { dispatch(deleteLikes(yourLike, msgListParams))
-  } else {
-    dispatch(profileDeleteLikes(yourLike, msgListParams, username));
-  }
+      }
+    }
+    if (!props.profile) {
+      dispatch(deleteLikes(yourLike, msgListParams))
+    } else {
+      dispatch(profileDeleteLikes(yourLike, msgListParams, username));
+    }
   };
 
- return (  <>
+  return (<>
 
-<div class="ui feed">
-  <div class="event">
-    <div class="label">
-    <img 
-          src = {`https://kwitter-api.herokuapp.com/users/${props.username}/picture`}
-          width="200" 
-          height="200"/>
-    </div>
-    <div class="content">
-      <div class="summary">
-        <a class="user">
-        {props.username}
-        </a> 
+    <div class="ui feed">
+      <div class="event">
+        <div class="label">
+          <img
+            src={props.userPhoto ? `https://kwitter-api.herokuapp.com/users/${props.username}/picture` : '/kwitter-user.png'}
+            width="200"
+            height="200" />
         </div>
-        <div class="summary">
+        <div class="content">
+          <div class="summary">
+            <a class="user">
+              {props.username}
+            </a>
+          </div>
+          <div class="summary">
             <a class="user">
             </a> {props.text}
             <div class="date">
-            {timestamp.time}<span> date:{timestamp.date}</span>
-        </div>
-      </div>
-      <div class="meta">
-      <div class="ui labeled button" tabIndex="0"> 
-            <button class="ui blue button" onClick={() => handleLike(props.msgId)}> {}
+              {timestamp.time}<span> date:{timestamp.date}</span>
+            </div>
+          </div>
+          <div class="meta">
+            <div class="ui labeled button" tabIndex="0">
+              <button class="ui blue button" onClick={() => handleLike(props.msgId)}> { }
                 <i class="heart icon"></i> Like(s)
             </button>
-            <a class="ui basic blue left pointing label">
-               {props.likes.length} 
-            </a>
-        </div>
-        <div class="ui labeled button" tabIndex="0"> 
-        <button class="ui blue button" onClick={() => handleUnlike()}>
-                 <i class="thumbs down icon"></i> Delete Like(s)
+              <a class="ui basic blue left pointing label">
+                {props.likes.length}
+              </a>
+            </div>
+            <div class="ui labeled button" tabIndex="0">
+              <button class="ui blue button" onClick={() => handleUnlike()}>
+                <i class="thumbs down icon"></i> Delete Like(s)
             </button>
-        </div>
-        <div class="ui labeled button" tabIndex="0">
-            <button class="ui red button" onClick={() => setDisClick(disClick + 1 )}>
+            </div>
+            <div class="ui labeled button" tabIndex="0">
+              <button class="ui red button" onClick={() => setDisClick(disClick + 1)}>
                 <i class="frown icon"></i> Dislikes
             </button>
-            <a class="ui basic left pointing red label">
+              <a class="ui basic left pointing red label">
                 {disClick}
-            </a>
-        </div>
-            {username === props.username  ? (
-        <div class='ui labeled button' tabIndex='0'>
-          <button
-            class='ui purple button'
-            onClick={() => handleDelete(props.msgId)}
-          >
-            <i class='cross mark icon'></i> Delete Message
+              </a>
+            </div>
+            {username === props.username ? (
+              <div class='ui labeled button' tabIndex='0'>
+                <button
+                  class='ui purple button'
+                  onClick={() => handleDelete(props.msgId)}
+                >
+                  <i class='cross mark icon'></i> Delete Message
           </button>
-        </div>
-      ) : null}
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-    </>)
+  </>)
 }
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -5,46 +5,46 @@ import { actions } from "../../redux/actions/auth";
 import "./Menu.css";
 
 export const Menu = () => {
-  const {isAuthenticated,username} = useSelector(state => ({isAuthenticated:!!state.auth.isAuthenticated, username:state.auth.username}));
+  const { isAuthenticated, username } = useSelector(state => ({ isAuthenticated: !!state.auth.isAuthenticated, username: state.auth.username }));
   const dispatch = useDispatch();
   const logout = () => dispatch(actions.logout());
 
   return (
     <div id="menu">
       <div id="kwitterhead">
-      <p>Kwitter</p>
+        <p>Kwitter</p>
       </div>
       <div class="ui horizontal list">
         <div class="item">
-          <img class="ui mini circular image" src={"https://kwitter-api.herokuapp.com/users/shanquetta/picture"} />
+          <img class="ui mini circular image" src={'/q.png'} />
           <div class="content">
             <div class="ui sub header">Shanquetta Pelzer</div>
             Product Owner
           </div>
         </div>
         <div class="item">
-          <img class="ui mini circular image" src={"https://kwitter-api.herokuapp.com/users/drisco/picture"} />
+          <img class="ui mini circular image" src={'/kwitter-user.png'} />
           <div class="content">
             <div class="ui sub header">Edward Perkins</div>
             Scrum Master
           </div>
         </div>
         <div class="item">
-          <img src={"https://kwitter-api.herokuapp.com/users/Elijah/picture"} class="ui mini circular image" />
+          <img src={'/elijah.jpg'} class="ui mini circular image" />
           <div class="content">
             <div class="ui sub header">Elijah Camara</div>
             QA
           </div>
         </div>
         <div class="item">
-          <img src={"https://kwitter-api.herokuapp.com/users/tawright28/picture"} class="ui mini circular image" />
+          <img src={'/tune.jpg'} class="ui mini circular image" />
           <div class="content">
             <div class="ui sub header">Tuan Wright</div>
             Developer
           </div>
         </div>
         <div class="item">
-          <img src={"https://kwitter-api.herokuapp.com/users/Tutwiler/picture"} class="ui mini circular image" />
+          <img src={"/charles.jpg"} class="ui mini circular image" />
           <div class="content">
             <div class="ui sub header">Charles Campbell</div>
             Developer
@@ -55,13 +55,13 @@ export const Menu = () => {
       <div id='menu-links'>
         {isAuthenticated ? (
           <>
-            <Link to={"/profile/"+username}>Profile</Link>
+            <Link to={"/profile/" + username}>Profile</Link>
             <Link to="/users">Users</Link>
             <Link to="/feed">Message Feed</Link>
             <Link to="/" onClick={logout}>
               Logout
             </Link>
-            
+
           </>
         ) : null}
       </div>
